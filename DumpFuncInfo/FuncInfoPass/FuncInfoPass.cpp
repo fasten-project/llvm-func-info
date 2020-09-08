7d06f44023f060cb02245e2bcc9b75bc9e0849e6
StefanosChaliasos <stefanoshaliassos@gmail.com>
2020-09-08 15:08:13 +0300

    Add licence

--- DumpFuncInfo/FuncInfoPass/FuncInfoPass.cpp
+++ DumpFuncInfo/FuncInfoPass/FuncInfoPass.cpp
@@ -1,3 +1,25 @@
+// Copyright (c) 2018-2020 FASTEN.
+//
+// This file is part of FASTEN
+// (see https://www.fasten-project.eu/).
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
 #include <iostream>
 
 #include <llvm/Pass.h>
@@ -22,7 +44,7 @@ struct FunctionInfoPass : public ModulePass {
     errs() << "function_name,function_type,directory,filename,static\n";
     for (Function &F : M.getFunctionList()) {
       dumpFunctionInfo(F);
-    } 
+    }
     return false;
   }
 
@@ -32,7 +54,7 @@ struct FunctionInfoPass : public ModulePass {
     }
     std::string info = F.getName().str();
     if (F.isDeclaration()) {
-      info += ",declaration,-"; 
+      info += ",declaration,-";
     }
     DISubprogram *subprog = F.getSubprogram();
     if (subprog) {
